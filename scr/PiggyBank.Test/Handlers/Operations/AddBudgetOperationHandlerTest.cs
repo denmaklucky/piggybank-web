7de09fc2cbdf10815a72d31e53da3efbbf055d69
Denis Makarenko <denis-makarenko@outlook.com>
2020-04-11 16:46:51 +0300

    Add test for BudgetOperation

--- scr/PiggyBank.Test/Handlers/Operations/AddBudgetOperationHandlerTest.cs
+++ scr/PiggyBank.Test/Handlers/Operations/AddBudgetOperationHandlerTest.cs
@@ -4,21 +4,19 @@ using PiggyBank.Common.Enums;
 using PiggyBank.Domain.Handler.Operations;
 using PiggyBank.Model;
 using PiggyBank.Model.Models.Entities;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace PiggyBank.Test.Handlers.Operations
 {
-    public class AddBudgetOperationHandlerTest
+    public class AddBudgetOperationHandlerTest : IDisposable
     {
         private readonly PiggyContext _context;
         public AddBudgetOperationHandlerTest()
-        {
-            _context = new PiggyContext(new DbContextOptionsBuilder<PiggyContext>()
-                .UseInMemoryDatabase(databaseName: "PiggyBank_InMemory")
-                .Options);
-        }
+            => _context = new PiggyContext(new DbContextOptionsBuilder<PiggyContext>()
+                .UseInMemoryDatabase(databaseName: "PiggyBank_InMemory").Options);
 
         [Fact]
         public async Task Invoke_OperationIsValid_OperationWasAdded()
@@ -32,13 +30,13 @@ namespace PiggyBank.Test.Handlers.Operations
                 Type = OperationType.Budget
             };
 
-            _context.Accounts.Add(new Account 
+            _context.Accounts.Add(new Account
             {
                 Id = 1,
                 Balance = 0
             });
 
-            _context.Categories.Add(new Category 
+            _context.Categories.Add(new Category
             {
                 Id = 1
             });
@@ -68,7 +66,106 @@ namespace PiggyBank.Test.Handlers.Operations
             };
 
             var handler = new AddBudgetOperationHandler(_context, addOperaion);
-            Assert.ThrowsAsync(handler.Invoke());
+            await Assert.ThrowsAsync<ArgumentException>(() => handler.Invoke());
+        }
+
+        [Fact]
+        public async Task Invoke_CategoryIdNotValid_ThrowsException()
+        {
+            var addOperaion = new AddBudgetOperationCommand
+            {
+                AccountId = 1,
+                Amount = 100,
+                CategoryId = 1,
+                Comment = "Hello, world",
+                Type = OperationType.Budget
+            };
+
+            _context.Accounts.Add(new Account
+            {
+                Id = 1,
+                Balance = 0
+            });
+
+            _context.SaveChanges();
+            var handler = new AddBudgetOperationHandler(_context, addOperaion);
+
+            await Assert.ThrowsAsync<ArgumentException>(() => handler.Invoke());
+        }
+
+        [Fact]
+        public async Task Invoke_WhenCallWithIncomeCategory_BalancePluse()
+        {
+            var addOperaion = new AddBudgetOperationCommand
+            {
+                AccountId = 1,
+                Amount = 100,
+                CategoryId = 1,
+                Comment = "Hello, world",
+                Type = OperationType.Budget
+            };
+
+            var account = new Account
+            {
+                Id = 1,
+                Balance = 0
+            };
+
+            _context.Accounts.Add(account);
+
+            _context.Categories.Add(new Category
+            {
+                Id = 1,
+                Type = CategoryType.Income
+            });
+
+            _context.SaveChanges();
+            var handler = new AddBudgetOperationHandler(_context, addOperaion);
+            await handler.Invoke();
+
+            Assert.Equal(100, account.Balance);
+        }
+
+        [Fact]
+        public async Task Invoke_WhenCallWithExpenseCategory_BalanceMinus()
+        {
+            var addOperaion = new AddBudgetOperationCommand
+            {
+                AccountId = 1,
+                Amount = 100,
+                CategoryId = 1,
+                Comment = "Hello, world",
+                Type = OperationType.Budget
+            };
+
+            var account = new Account
+            {
+                Id = 1,
+                Balance = 0
+            };
+
+            _context.Accounts.Add(account);
+
+            _context.Categories.Add(new Category
+            {
+                Id = 1,
+                Type = CategoryType.Expense
+            });
+
+            _context.SaveChanges();
+            var handler = new AddBudgetOperationHandler(_context, addOperaion);
+            await handler.Invoke();
+
+            Assert.Equal(-100, account.Balance);
+        }
+
+        public void Dispose()
+        {
+            _context.Accounts.RemoveRange(_context.Accounts);
+            _context.Categories.RemoveRange(_context.Categories);
+            _context.Operations.RemoveRange(_context.Operations);
+            _context.SaveChanges();
+            _context.Dispose();
         }
     }
 }
