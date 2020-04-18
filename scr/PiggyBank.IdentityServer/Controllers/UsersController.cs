5208d367b7eb6abcb750cf19f74c5035d505b9e3
Denis Makarenko <denis-makarenko@outlook.com>
2020-04-18 14:21:53 +0300

    Add IdentityUserManger

---
+++ scr/PiggyBank.IdentityServer/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PiggyBank.IdentityServer.Dto;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PiggyBank.IdentityServer.Controllers
+{
+    [ApiController, Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post(UserDto request, CancellationToken token)
+        {
+            await Task.Delay(100);
+            return Ok();
+        }
+    }
+}
---
+++ scr/PiggyBank.IdentityServer/Dto/UserDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PiggyBank.IdentityServer.Dto
+{
+    public class UserDto
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
