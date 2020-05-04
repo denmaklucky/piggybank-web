651fc8d1e95991f7e9615e7c59e8f238052bb6a6
Denis Makarenko <denis-makarenko@outlook.com>
2020-05-04 13:13:36 +0300

    Add selected item when you select navigationMenu

---
+++ scr/PiggyBank.WebSite/Models/Components/NavigationLinkModel.cs
@@ -0,0 +1,13 @@
+﻿namespace PiggyBank.WebSite.Models.Components
+{
+    public class NavigationLinkModel
+    {
+        public string Href { get; set; }
+
+        public string LinkText { get; set; }
+
+        public string ImgClass { get; set; }
+
+        public bool IsSelected { get; set; }
+    }
+}
